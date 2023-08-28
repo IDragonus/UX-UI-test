8e06a66a56d7eb6a2315126c0442b2ac5d8fce05
ruben <ralarcon@labsxd.com>
2023-08-28 00:26:38 -0500

    feat: beginnig the footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
 })
 export class FooterComponent {}
